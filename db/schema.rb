0dfd3f6c0fe66fba28ec7fc01058521050d6b18d
Doug Black Jr <dougblackjr@gmail.com>
2016-07-24 11:54:58 -0400

    Added User modules

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160724154325) do
+ActiveRecord::Schema.define(version: 20160724155228) do
 
   create_table "beds", force: :cascade do |t|
     t.integer  "bedNumber"
@@ -117,5 +117,23 @@ ActiveRecord::Schema.define(version: 20160724154325) do
   end
 
   add_index "residents", ["place_id"], name: "index_residents_on_place_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
